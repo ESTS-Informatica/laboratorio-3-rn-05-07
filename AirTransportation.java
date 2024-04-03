0586be4a46878a6ec96d4bf5ad76fccefd9f553c
Miguel Certã <112079507+mpcsource@users.noreply.github.com>
2024-04-03 08:46:23 +0100

    Nível 1 - AirTransportation

---
+++ AirTransportation.java
@@ -0,0 +1,12 @@
+
+/**
+ * Escreva uma descrição da classe AirTransportation aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class AirTransportation extends Transport
+{
+    private String name;
+    private int numberOfContainers;
+}
