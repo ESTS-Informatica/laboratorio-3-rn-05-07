b4eccfbe1d5087f23555913a48f65ac62754efcf
diox0843 <153206798+diox0843@users.noreply.github.com>
2024-04-03 09:01:38 +0100

    GroundTransportation nivel 1 e 2
    Feito na totalidade o nível 2 e completo no setFees no nível 1.

--- GroundTransportation.java
+++ GroundTransportation.java
@@ -2,9 +2,17 @@ public class GroundTransportation extends Transport
 {
     // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
     private String licensePlate;
-    public GroundTransportation()
+    public GroundTransportation(String licensePlate)
     {
-        
+        this.licensePlate = licensePlate;
+        super.setFees(3.0);
     }
-
+    public String getLicensePlate(){
+        return licensePlate;
+    }
+    public void setLicensePlate(String licensePlate){
+        this.licensePlate = licensePlate;
+    }
 }
+
+
