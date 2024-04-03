a387adfe6cb00d903f2d67672f0ff7249ea9f011
Miguel Certã <112079507+mpcsource@users.noreply.github.com>
2024-04-03 09:01:31 +0100

    Nível 1 e 2 - AirTransportation
    (Só na AirTransportation)
    Nível 2 feito na totalidade, e completo o setFees para o nível 1.

--- AirTransportation.java
+++ AirTransportation.java
@@ -9,4 +9,16 @@ public class AirTransportation extends Transport
 {
     private String name;
     private int numberOfContainers;
+    
+    public AirTransportation(String name, int numberOfContainers){
+        super.setFees(4.0);
+        this.name = name;
+        this.numberOfContainers = numberOfContainers;
+    }
+    
+    public String getName() { return name; }
+    public int getNumberOfContainers() { return numberOfContainers; }
+
+    public void setName(String name) { this.name = name; }
+    public void setNumberOfContainers(int numberOfContainers) { this.numberOfContainers = numberOfContainers; }
 }
