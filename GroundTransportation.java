eb81dc62d3cd27ec0e9f18e8dfe983635e93bf64
diox0843 <153206798+diox0843@users.noreply.github.com>
2024-04-03 08:47:08 +0100

    Nível 1 - GroundTransportation

---
+++ GroundTransportation.java
@@ -0,0 +1,10 @@
+public class GroundTransportation extends Transport
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private String licensePlate;
+    public GroundTransportation()
+    {
+        
+    }
+
+}
