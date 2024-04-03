9643c21ab780a53e264831d8c9b0a61a59f164a0
diox0843 <153206798+diox0843@users.noreply.github.com>
2024-04-03 09:30:06 +0100

    Nível 1 e 3 Ground Transportation
    Adicionar constante Ground Fees
    Nivel 3 completo

--- GroundTransportation.java
+++ GroundTransportation.java
@@ -2,16 +2,31 @@ public class GroundTransportation extends Transport
 {
     // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
     private String licensePlate;
+    private final static double GROUND_FEES = 3;
     public GroundTransportation(String licensePlate)
     {
         this.licensePlate = licensePlate;
-        super.setFees(3.0);
+        setFees(GROUND_FEES);
     }
     public String getLicensePlate(){
         return licensePlate;
     }
     public void setLicensePlate(String licensePlate){
         this.licensePlate = licensePlate;
+    }
+    public double getPriceWithFees(){
+        return super.getPriceWithFees();
+    }
+    @Override
+    public String getTransportType(){
+        return "Transporte Terrestre";
+    }
+    
+    public String toString(){
+        final StringBuilder sb = new StringBuilder();
+        sb.append(super.toString());
+        sb.append(String.format("%15s: %s\n", "Matricula", getLicensePlate()));
+        return sb.toString();
     }
 }
 
